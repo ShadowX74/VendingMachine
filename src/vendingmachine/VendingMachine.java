db170e1aae27dbe5a64ddc997b2893e37c6f1ba2
ShadowX74 <xshen2013@gmail.com>
2017-04-21 20:50:04 -0500

    Finished Vending Machine

--- src/vendingmachine/VendingMachine.java
+++ src/vendingmachine/VendingMachine.java
@@ -8,6 +8,9 @@ package vendingmachine;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,6 +22,7 @@ public class VendingMachine {
      * @param args the command line arguments
      */
     static ArrayList<Candy>[][] machine = new ArrayList[3][3];
+    static Scanner scan;
 
     public static void main(String[] args) {
         stockMachine();
@@ -29,7 +33,7 @@ public class VendingMachine {
         for (int k = 0; k < machine.length; k++) {
             for (int i = 0; i < machine[0].length; i++) {
                 Random rand = new Random();
-                int candies = rand.nextInt(10) + 1;
+                int candies = rand.nextInt(9) + 1;
                 machine[k][i] = new ArrayList<Candy>();
                 for (int j = 0; j < candies; j++) {
                     Candy c = null;
@@ -65,18 +69,116 @@ public class VendingMachine {
     }
 
     private static void askUser() {
-        printMachine();
-        System.out.println("What would you like to buy?");
+        boolean play = true;
+        while (play) {
+            printMachine();
+            System.out.println("What would you like to buy?");
+            System.out.println("Insert letter, then the number.");
+
+            scan = new Scanner(System.in);
+            String action = scan.nextLine().toLowerCase();
+            
+            System.out.println("\n\n\n");
+            if (action.length() >= 2) {
+                String letter = action.substring(0, 1);
+                String number = action.substring(1, 2);
+                if (action.equals("quit")) {
+                    play = false;
+                } else if (letter.equals("a")) {
+                    if (number.equals("1")) {
+                        if (machine[0][0].isEmpty()) {
+                            System.out.println("This candy has run out, please make another selection.");
+                        } else {
+                            System.out.println(machine[0][0].get(0).toString());
+                            machine[0][0].remove(0);
+                        }
+                    } else if (number.equals("2")) {
+                        if (machine[0][1].isEmpty()) {
+                            System.out.println("This candy has run out, please make another selection.");
+                        } else {
+                            System.out.println(machine[0][1].get(0).toString());
+                            machine[0][1].remove(0);
+                        }
+                    } else if (number.equals("3")) {
+                        if (machine[0][2].isEmpty()) {
+                            System.out.println("This candy has run out, please make another selection.");
+                        } else {
+                            System.out.println(machine[0][2].get(0).toString());
+                            machine[0][2].remove(0);
+                        }
+                    }
+                } else if (letter.equals("b")) {
+                    if (number.equals("1")) {
+                        if (machine[1][0].isEmpty()) {
+                            System.out.println("This candy has run out, please make another selection.");
+                        } else {
+                            System.out.println(machine[1][0].get(0).toString());
+                            machine[1][0].remove(0);
+                        }
+                    } else if (number.equals("2")) {
+                        if (machine[1][1].isEmpty()) {
+                            System.out.println("This candy has run out, please make another selection.");
+                        } else {
+                            System.out.println(machine[1][1].get(0).toString());
+                            machine[1][1].remove(0);
+                        }
+                    } else if (number.equals("3")) {
+                        if (machine[1][2].isEmpty()) {
+                            System.out.println("This candy has run out, please make another selection.");
+                        } else {
+                            System.out.println(machine[1][2].get(0).toString());
+                            machine[1][2].remove(0);
+                        }
+                    }
+                } else if (letter.equals("c")) {
+                    if (number.equals("1")) {
+                        if (machine[2][0].isEmpty()) {
+                            System.out.println("This candy has run out, please make another selection.");
+                        } else {
+                            System.out.println(machine[2][0].get(0).toString());
+                            machine[2][0].remove(0);
+                        }
+                    } else if (number.equals("2")) {
+                        if (machine[2][1].isEmpty()) {
+                            System.out.println("This candy has run out, please make another selection.");
+                        } else {
+                            System.out.println(machine[2][1].get(0).toString());
+                            machine[2][1].remove(0);
+                        }
+                    } else if (number.equals("3")) {
+                        if (machine[2][2].isEmpty()) {
+                            System.out.println("This candy has run out, please make another selection.");
+                        } else {
+                            System.out.println(machine[2][2].get(0).toString());
+                            machine[2][2].remove(0);
+                        }
+                    }
+                } else {
+                    System.out.println("Invalid.");
+                }
+            } else {
+                System.out.println("Invalid.");
+            }
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(VendingMachine.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("\n\n\n");
+        }
     }
 
     private static void printMachine() {
-        System.out.println("   123");
-        System.out.println("   __________________________");
-        System.out.println("1 |" + machine[0][0].get(0).name + "|" + machine[0][1].get(0).name + "   |" + machine[0][2].get(0).name + "\t|");
-        System.out.println("  |_____________|____________|_____________|");
-        System.out.println("2 |" + machine[1][0].get(0).name + "\t|" + machine[1][1].get(0).name + "   |" + machine[1][2].get(0).name + "\t|");
-        System.out.println("  |_____________|____________|______|");
-        System.out.println("3 |" + machine[2][0].get(0).name + "\t|" + machine[2][1].get(0).name + "|" + machine[2][2].get(0).name + "\t|");
-        System.out.println("  |_____________|____________|______|");
+        System.out.println("         1              2             3");
+        System.out.println("   ____________________________________________");
+        System.out.println("A |Triple Bubble|   Himshey's  |   200 Grand   |");
+        System.out.println("  |\t " + machine[0][0].size() + "\t|       " + machine[0][1].size() + "      |       " + machine[0][2].size() + "       |");
+        System.out.println("  |_____________|______________|_______________|");
+        System.out.println("B |  Chuckles   |   Andromeda  |  Gummy Bears  |");
+        System.out.println("  |\t " + machine[1][0].size() + "\t|       " + machine[1][1].size() + "      |       " + machine[1][2].size() + "       |");
+        System.out.println("  |_____________|______________|_______________|");
+        System.out.println("C |  Dit Dogs   | Tarty Sweets |Smiling Cowboys|");
+        System.out.println("  |\t " + machine[2][0].size() + "\t|       " + machine[2][1].size() + "      |       " + machine[2][2].size() + "       |");
+        System.out.println("  |_____________|______________|_______________|");
     }
 }
